f40e7bf2c11b153251b849e40379235af4d8c48b
Xavier-Labarbe <xavier.labarbe@viacesi.fr>
2022-06-28 06:34:02 +0200

    Added Orders

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, response, Response } from "express";
 import axios from "axios";
 const cors = require("cors");
+const bodyParser = require('body-parser');
 
 const app: Application = express();
 const port: number = 3000;
@@ -8,6 +9,7 @@ const port: number = 3000;
 app.options("*", cors({ origin: "*", optionsSuccessStatus: 200 }));
 
 app.use(cors({ origin: "*", optionsSuccessStatus: 200 }));
+app.use(bodyParser.json());
 
 app.get("/getHome", async (req: Request, res: Response) => {
   const allRestaurants = await axios.get(
@@ -31,6 +33,14 @@ app.get("/getRestaurant/:name", async (req: Request, res: Response) => {
   );
   res.json(restaurant.data);
 });
+
+app.post("/addOrder", async (req: Request, res: Response) => {
+  console.log(req.body);
+  const order = await axios.post(
+    "http://localhost:3001/addOrder", req.body
+  );
+  res.json(order.data);
+})
 
 app.listen(port, () => {
   console.log(`Veat-Passerelle listening on port ${port}`);
